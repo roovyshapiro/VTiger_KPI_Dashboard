89e3c2096448e662708716ec9724de292d5d043e
Roovy Shapiro <roovyshapiro@gmail.com>
2020-02-05 15:57:15 -0500

    Add refresh count down
    When the "auto-refresh" is checked,
    A number will now be displayed next to it showing how much time
    is left before the page is refreshed.

--- sales_dashboard/dashboard/static/dashboard/scripts.js
+++ sales_dashboard/dashboard/static/dashboard/scripts.js
@@ -1,9 +1,12 @@
-// AUTO-UPDATE CHECKBOX 
+/* -----  AUTO-UPDATE CHECKBOX  -----*/
 
+//The amount of time in seconds until the page reloads
+var update_time = 20;
 //After a page reload, set the checkbox status to what's saved in localStorage
 window.onload = function set_check_box() {
     var checked_status = JSON.parse(localStorage.getItem("checkbox_autoupdate"));
     document.getElementById("checkbox_autoupdate").checked = checked_status;
+    localStorage.setItem("refresh_count", (this.update_time));
     autorefresh();
 }
 //Saves the current checkbox state to localStorage
@@ -16,15 +19,30 @@ function autorefresh() {
     save();
     var isChecked = document.getElementById("checkbox_autoupdate").checked;
     if (isChecked == true) {
+        refresh_counter();
         time = setInterval(function () {
             window.location.assign("http://127.0.0.1:8000/populate");
-        }, 600000);
-
+        }, (update_time * 1000));
     } else if (isChecked == false) {
         clearInterval(time);
+        clearTimeout(timeoutVar);
+        document.getElementById("auto_update_label").innerHTML = 'Auto Refresh';
+        localStorage.setItem("refresh_count", (this.update_time));
     }
 }
 autorefresh();
 document.getElementById('checkbox_autoupdate').addEventListener('click', autorefresh);
+
+//This function displays how many seconds remain until the page is auto-refreshed
+function refresh_counter() {
+        var refresh_count_int = JSON.parse(localStorage.getItem("refresh_count"));
+        refresh_count_int = refresh_count_int - 1;
+        document.getElementById("auto_update_label").innerHTML = 'Auto Refresh: ' + refresh_count_int;
+        localStorage.setItem("refresh_count", refresh_count_int);
+        if (refresh_count_int > 0){
+            console.log(refresh_count_int)
+            timeoutVar = setTimeout(refresh_counter, 1000);
+        }
+}
 
 
