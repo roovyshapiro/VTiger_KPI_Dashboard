e969d56c24554da6ad3af82f7f6478bff50404a6
Roovy Shapiro <roovyshapiro@gmail.com>
2020-02-12 10:37:02 -0500

    Add Bash Script to start all services

---
+++ sales_dashboard/startapps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+redis-server &
+celery -A sales_dashboard worker &
+celery -A sales_dashboard beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+python3 manage.py runserver
