736a1f015bc341252fd956fe4eb3430422822962
Roovy Shapiro <roovyshapiro@gmail.com>
2020-02-27 16:21:41 -0500

    Restructuring tasks.py to disregard update times
    There's no need to add items from the database together, since
    we're no longer getting data from only the past ten minutes.
    Instead, we're getting total numbers from the entire day and then
    just replacing the values in the items from the database.

--- sales_dashboard/dashboard/tasks.py
+++ sales_dashboard/dashboard/tasks.py
@@ -8,16 +8,17 @@ import VTiger_Sales_API
 import json, os
 
 @shared_task
-def test_celery_beat():
-    print('is this working?')
-
-
-@shared_task
 def populate_db_celery():
     '''
     Populates the database with stats from vtigerapi.retrieve_data()
-    demo_scheduled, demo_given, quote_sent, pilot, needs_analysis, closed_won, closed_lost, phone_calls, date, user
-    {james_frinkle:[0, 1, 15, 0, 0, 3, 6, '215', '2020-01-28 21:30:00', 'james_frinkle']}
+
+    We retrieve a dictionary from VTiger that is formatted like this:
+    {'gareth_bunkard': [0, 0, 0, 0, 0, 0, 0, '0', '2020-02-27 05:00:00', 'gareth_bunkard'], 
+    'salvadore_louise': [0, 1, 3, 0, 0, 1, 0, '28', '2020-02-27 05:00:00', 'salvadore_louise'], 
+    'shiminy_cartwheel': [0, 0, 0, 0, 0, 0, 0, '95', '2020-02-27 05:00:00', 'shiminy_cartwheel'], 
+    'johnny_flinkson': [2, 1, 0, 1, 0, 0, 0, '74', '2020-02-27 05:00:00', 'johnny_flinkson']}
+
+    {username: [demo scheduled, demo given, quote sent, pilot, needs analysis, closed won, closed lost, phone calls, date to start pulling data from, username']}
     '''
     user_stat_dict = retrieve_stats()
     stats = Sales_stats.objects.all()
@@ -26,27 +27,21 @@ def populate_db_celery():
     today_end = timezone.now().replace(hour=23, minute=59, second=59, microsecond=0) 
 
     for value in user_stat_dict.values():
+        #Attempt to find data in the database from today, if no objects are found, create new ones.
         sales_stat_query = stats.filter(user = value[9], date_created__gte=today_start, date_created__lte=today_end)
-        #If there are no objects in the database, create new ones.
         if len(sales_stat_query) == 0:
             stat = Sales_stats()
-            stat.demo_scheduled = value[0]
-            stat.demo_given = value[1]
-            stat.quote_sent = value[2]
-            stat.pilot = value[3]
-            stat.needs_analysis = value[4]
-            stat.closed_won = value[5]
-            stat.closed_lost = value[6]
         #If objects exists for today, update the existing ones instead of creating new ones.
         else:
             stat = sales_stat_query[0]
-            stat.demo_scheduled = str(int(stat.demo_scheduled) + value[0])
-            stat.demo_given = str(int(stat.demo_given) + value[1])
-            stat.quote_sent = str(int(stat.quote_sent) + value[2])
-            stat.pilot = str(int(stat.pilot) + value[3])
-            stat.needs_analysis = str(int(stat.needs_analysis) + value[4])
-            stat.closed_won = str(int(stat.closed_won) + value[5])
-            stat.closed_lost = str(int(stat.closed_lost) + value[6])
+
+        stat.demo_scheduled =  value[0]
+        stat.demo_given = value[1]
+        stat.quote_sent  = value[2]
+        stat.pilot  = value[3]
+        stat.needs_analysis = value[4]
+        stat.closed_won = value[5]
+        stat.closed_lost =  value[6]
 
         stat.date = value[8]
         stat.user = value[9]
@@ -74,26 +69,12 @@ def retrieve_stats():
     It should have the following format:
     {"username": "(user)", "access_key": "(access_key)", "host": "(host)"}
     '''
-    #Get the most recently added item to the database
-    #and retrieve data from that point. The time frame for when
-    #to retrieve new data is decided by the celery beat period task.
-    #Wether its every 1, 5 or 10 minutes, this function will return
-    #all the data point since the last entry in the database.
-    try:
-        latest_item = Sales_stats.objects.latest('date_modified')
-        update_timespan = latest_item.date_modified
-    except:
-        #If the database is empty, then the most recent item can't be checked
-        #We pass 'today' which vtigerapi.retrieve_data will use to gather
-        #all data since the beginning of the day.
-        update_timespan = 'today'    
-
     credentials_file = 'credentials.json'
     credentials_path = os.path.join(os.path.abspath('.'), credentials_file)
     with open(credentials_path) as f:
         data = f.read()
     credential_dict = json.loads(data)
     vtigerapi = VTiger_Sales_API.Vtiger_api(credential_dict['username'], credential_dict['access_key'], credential_dict['host'])
-    user_stat_dict = vtigerapi.retrieve_data(update_timespan)
+    user_stat_dict = vtigerapi.retrieve_data()
 
     return user_stat_dict
