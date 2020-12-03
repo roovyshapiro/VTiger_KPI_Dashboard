8f15cdae118d2a26aa8e3e8fcc0a07c38d0259c6
Roovy Shapiro <roovyshapiro@gmail.com>
2020-12-03 13:05:13 -0500

    Removed End Date Selector - Date overhaul
    Made a significant number of changes in this commit.
    The cases that are presented no longer fit into a specified range.
    Instead the user will select a single day and cases will be presented
    for that specific day. If no date is selected, then it uses today's
    current date as the default. The next step is to show the default
    data for the current week and the current month.

--- sales_dashboard/case_dashboard/case_views.py
+++ sales_dashboard/case_dashboard/case_views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.utils import timezone
 from django.db.models import Q
-
+import datetime
 from .tasks import populate_db_celery_cases
 from .models import Cases
+
 
 def main_dashboard(request):
     '''
@@ -11,96 +12,68 @@ def main_dashboard(request):
     Send all unique group names in the context so that it can be used in a dropdown.
     Get the data from the form inputs like "group name" and filter the cases to be displayed.
     '''
-    #Returns all cases with the most recently modified first if no filter is applied
+    #Prepare the date and group that was chosen, so it can be displayed on the dashboard
+    date_group_dict = {}
+
     full_cases = Cases.objects.all().order_by('-modifiedtime')
+
     #Returns dictionary with "assigned_groupname" as key, and the actual name as the value.
     #We pass these groups to the html to populate the drop down menu for filtering
     case_groups = Cases.objects.values('assigned_groupname').distinct()
 
     #We get the form submission from the drop down and use it to then filter the "full_case"
-    #query set to only return and display the cases which match that group.
+    #query set to only return and display the cases which match that group. If no group is 
+    #selected, data is displayed from all groups and "All Groups" is sent to be displayed.
     group_request = request.GET.get('group_dropdown')
     if group_request != '' and group_request is not None and group_request != '--Select Group--':
         full_cases = full_cases.filter(assigned_groupname=group_request)
+        date_group_dict['group'] = group_request
 
-    date_start_request = request.GET.get('date_start')
-    date_end_request = request.GET.get('date_end')
-
-    if date_start_request != '' and date_start_request is not None:
-        full_cases = full_cases.filter(modifiedtime__gte=date_start_request)
-
-    if date_end_request != '' and date_end_request is not None:
-        full_cases = full_cases.filter(modifiedtime__lt=date_end_request)
-
-    #Prepare the date and group that was chosen, so it can be displayed on the dashboard
-    date_group_dict = {}
-    date_group_dict['date_start'] = date_start_request
-    date_group_dict['date_end'] = date_end_request
-    date_group_dict['group'] = group_request
-
-    #If no date was selected, we'll display the time frame from the first modified case to the last
-    #We use the custom "modifiedtime_date" method so we can display the time in the same format as the
-    #date picker: "modifiedtime.strftime('%Y-%m-%d')"
-    if group_request == '' or group_request == None or group_request == '--Select Group--':
+    else:
         date_group_dict['group'] = 'All Groups'
-    if date_start_request == '' or date_start_request == None:
-        first_case = full_cases.order_by('modifiedtime').first()
-        date_group_dict['date_start'] = first_case.modifiedtime_date
-    if date_end_request == '' or date_end_request == None:
-        last_case = full_cases.order_by('modifiedtime').last()
-        date_group_dict['date_end'] = last_case.modifiedtime_date
 
+    today, first_of_week, first_of_month = retrieve_dates()
+
+    date_request = request.GET.get('date_start')
+    if date_request == '' or date_request == None:
+        date_request = today
+    else:
+        date_request = datetime.datetime.strptime(date_request, '%Y-%m-%d')
+
+    date_request_end = date_request.replace(hour=23, minute = 59, second = 59, microsecond = 0)
+    
+    date_group_dict['date_start'] = date_request.strftime('%A, %B %d')
 
     #Prepare calculated data to present as a simple summary overview of the cases
+    full_cases = full_cases.filter(modifiedtime__gte=date_request, modifiedtime__lte=date_request_end)
     case_stats_dict = {}
-    case_stats = full_cases
 
-    #These calculations are based on not having any date selected
-    if date_start_request == '' or date_start_request is  None or date_end_request == '' or date_end_request is None:
-        case_stats_closed = case_stats.filter(Q(casestatus="Resolved") | Q(casestatus="Closed"))
-        case_stats_resolved = case_stats_closed.filter(case_resolved__gte=first_case.modifiedtime, case_resolved__lte=last_case.modifiedtime)
-
-        case_stats_dict['closed'] = len(case_stats_resolved)
-
-        case_stats_opened = case_stats.filter(createdtime__gte=first_case.modifiedtime, createdtime__lte=last_case.modifiedtime)
+    try:
+        case_stats_opened = full_cases.filter(createdtime__gte=date_request, createdtime__lte=date_request_end)
         case_stats_dict['opened'] = len(case_stats_opened)
+    except ValueError:
+        case_stats_dict['opened'] = 0
 
+    try:
+        case_stats_modified = full_cases.filter(case_resolved__gte=date_request, case_resolved__lte=date_request_end)
+        case_stats_closed = case_stats_modified.filter(Q(casestatus="Resolved") | Q(casestatus="Closed"))
+        case_stats_dict['closed'] = len(case_stats_closed)
+    except ValueError:
+        case_stats_dict['closed'] = 0
+
+    try:
+        case_stats = full_cases.filter(modifiedtime__gte=date_request, modifiedtime__lte=date_request_end)
         case_stats_dict['modified'] = len(case_stats)
+    except ValueError:
+        case_stats_dict['modified'] = 0
 
-        try:
-            case_stats_dict['kill_rate'] = int((case_stats_dict['closed'] / case_stats_dict['opened']) * 100)
-        except ZeroDivisionError:
-            #If there are 0 opened cases and 3 closed cases, the kill rate will become 300%.
-            case_stats_dict['kill_rate'] = int(case_stats_dict['closed'] * 100)
-
-    #If a date is selected, the calculations should only affect that time frame.
-    if date_start_request != '' and date_start_request is not None and date_end_request != '' and date_end_request is not None:
-        try:
-            case_stats_opened = case_stats.filter(createdtime__gte=date_start_request, createdtime__lte=date_end_request)
-            case_stats_dict['opened'] = len(case_stats_opened)
-        except ValueError:
-            case_stats_dict['opened'] = 0
-
-        try:
-            case_stats_modified = case_stats.filter(case_resolved__gte=date_start_request, case_resolved__lte=date_end_request)
-            case_stats_closed = case_stats_modified.filter(Q(casestatus="Resolved") | Q(casestatus="Closed"))
-            case_stats_dict['closed'] = len(case_stats_closed)
-        except ValueError:
-            case_stats_dict['closed'] = 0
-
-        try:
-            case_stats = case_stats.filter(modifiedtime__gte=date_start_request, modifiedtime__lte=date_end_request)
-            case_stats_dict['modified'] = len(case_stats)
-        except ValueError:
-            case_stats_dict['modified'] = 0
-
-        try:
-            case_stats_dict['kill_rate'] = int((case_stats_dict['closed'] / case_stats_dict['opened']) * 100)
-        except ValueError:
-            case_stats_dict['kill_rate'] = int(0)
-        except ZeroDivisionError:
-            #If there are 0 opened cases and 3 closed cases, the kill rate will become 300%.
-            case_stats_dict['kill_rate'] = int(case_stats_dict['closed'] * 100)
+    try:
+        case_stats_dict['kill_rate'] = int((case_stats_dict['closed'] / case_stats_dict['opened']) * 100)
+    except ValueError:
+        case_stats_dict['kill_rate'] = int(0)
+    except ZeroDivisionError:
+        #If there are 0 opened cases and 3 closed cases, the kill rate will become 300%.
+        case_stats_dict['kill_rate'] = int(case_stats_dict['closed'] * 100)
 
     #We calculate how many cases were closed per user and add it to the context to be displayed
     #all_users = <QuerySet [{'assigned_username': 'James Fulcrumstein'}, {'assigned_username': 'Mary Littlelamb'}]
@@ -112,7 +85,8 @@ def main_dashboard(request):
         user_dict[user['assigned_username']] = 0
 
     #user_dict = {'James Fulcrumstein':3, 'Mary Littlelamb':5, 'Kent Breakfield':0}
-    for case in full_cases.filter(Q(casestatus="Resolved") | Q(casestatus="Closed")):
+    case_stats_modified = full_cases.filter(case_resolved__gte=date_request, case_resolved__lte=date_request_end)
+    for case in case_stats_modified.filter(Q(casestatus="Resolved") | Q(casestatus="Closed")):
         if case.assigned_username in user_dict:
             user_dict[case.assigned_username] += 1
 
