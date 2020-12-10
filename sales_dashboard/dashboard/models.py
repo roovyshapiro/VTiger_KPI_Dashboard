f696abc68ab22a0ccfe76a54ee384ac914d72518
Roovy Shapiro <roovyshapiro@gmail.com>
2020-12-10 12:55:09 -0500

    Added opportunity model

--- sales_dashboard/dashboard/models.py
+++ sales_dashboard/dashboard/models.py
@@ -4,7 +4,6 @@ import VTiger_Sales_API
 import json, os
 
 
-# Create your models here.
 class Sales_stats(models.Model):
     '''
     All text fields:
@@ -114,7 +113,6 @@ class Sales_stats(models.Model):
 
         return user_score
 
-# Create your models here.
 class Phone_calls(models.Model):
     '''
     Phone calls can be retrieved as a total number for the day, as the number doesn't change.
@@ -128,3 +126,99 @@ class Phone_calls(models.Model):
 
     def __str__(self):
         return f'{self.user}: {self.phone_calls} - {self.date_modified.strftime("%Y-%m-%d %H:%M:%S")}'
+
+
+class Opportunity(models.Model):
+    '''
+    Example Opportunity:
+    {
+        "adjusted_amount": "999.00000000",
+        "amount": "999.00000000",
+        "assigned_groupname": "",
+        "assigned_user_id": "19x55",
+        "assigned_username": "James Johnkberg",
+        "campaignid": "",
+        "cf_potentials_chancetoclose": "Medium",
+        "cf_potentials_closedlostchangedat": "",
+        "cf_potentials_closedwonchangedat": "",
+        "cf_potentials_demogivenchangedat": "12-08-2020 03:31 PM",
+        "cf_potentials_demogivendate": "",
+        "cf_potentials_demonotes": "",
+        "cf_potentials_demoscheduledchangedat": "12-08-2020 03:27 PM",
+        "cf_potentials_discoverynotes": "Looking to buy a product!",
+        "cf_potentials_distributionid": "",
+        "cf_potentials_industry": "",
+        "cf_potentials_leadreferencenumber": "",
+        "cf_potentials_needsanalysischangedat": "12-10-2020 10:36 AM",
+        "cf_potentials_nextattempt": "2020-12-09",
+        "cf_potentials_pilotchangedat": "",
+        "cf_potentials_qualifiedby": "",
+        "cf_potentials_quotesentchangedat": "12-08-2020 04:03 PM",
+        "cf_potentials_timezone": "",
+        "cf_potentials_website": "",
+        "closingdate": "2021-01-21",
+        "contact_id": "4x929209",
+        "created_user_id": "19x55",
+        "createdtime": "2020-12-08 21:27:04",
+        "current_stage_entry_time": "2020-12-10 16:36:34",
+        "description": "Customer is looking to buy a product.",
+        "email": "",
+        "forecast_amount": "849.15000000",
+        "forecast_category": "Pipeline",
+        "id": "5x929210",
+        "isclosed": "0",
+        "isconvertedfromlead": "1",
+        "last_contacted_on": "",
+        "last_contacted_via": "",
+        "leadsource": "",
+        "lost_reason": "",
+        "modifiedby": "19x55",
+        "modifiedtime": "2020-12-10 16:36:35",
+        "nextstep": "",
+        "opportunity_type": "",
+        "pipeline": "Standard Sales Pipeline",
+        "potential_no": "POT2122",
+        "potentialname": "New Amazing Opportunity!",
+        "prev_sales_stage": "",
+        "probability": "85.000",
+        "related_to": "3x929208",
+        "sales_stage": "Needs Analysis",
+        "source": "CRM",
+        "starred": "",
+        "tags": ""
+    },
+    '''
+    opp_id = models.CharField(max_length=50)
+    contact_id = models.CharField(max_length=50)
+
+    opp_no = models.CharField(max_length=50)
+    opp_name = models.CharField(max_length=250)
+    opp_stage = models.CharField(max_length=50)
+
+    createdtime = models.DateTimeField()
+    modifiedtime = models.DateTimeField()
+
+    created_user_id = models.CharField(max_length=50)
+    modifiedby = models.CharField(max_length=50)
+    assigned_user_id = models.CharField(max_length=50)
+
+    assigned_username = models.CharField(max_length=75)
+    assigned_groupname = models.CharField(max_length=75)
+
+    current_stage_entry_time = models.DateTimeField()
+    demo_scheduled_changed_at = models.DateTimeField()
+    demo_given_changed_at = models.DateTimeField()
+    quote_sent_changed_at = models.DateTimeField()
+    pilot_changed_at = models.DateTimeField()
+    needs_analysis_changed_at = models.DateTimeField()
+    closed_lost_changed_at = models.DateTimeField()
+    closed_won_changed_at = models.DateTimeField()
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.assigned_username} - {self.opp_name} - {self.opp_no} - {self.date_modified.strftime("%Y-%m-%d %H:%M:%S")}'
+
+    def modifiedtime_date(self):
+        return self.modifiedtime.strftime('%Y-%m-%d')
