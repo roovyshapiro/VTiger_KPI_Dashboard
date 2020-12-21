8fc1c1d9dd05afda5bc464b04dcdb18373c99eb0
Roovy Shapiro <roovyshapiro@gmail.com>
2020-12-21 14:37:40 -0500

    Customize user list display in /admin

--- VTiger_KPI_Dashboard/home/admin.py
+++ VTiger_KPI_Dashboard/home/admin.py
@@ -1,3 +1,9 @@
+#Changes the way the list of Users is displayed in /admin/auth/user
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_active', 'last_login', 'date_joined']
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
