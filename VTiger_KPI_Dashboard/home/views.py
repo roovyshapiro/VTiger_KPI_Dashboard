01a6ca14a04f798c33f54b4290890ec397a11fc0
Roovy Shapiro <roovyshapiro@gmail.com>
2020-12-24 09:17:09 -0500

    (WIP) custom 404,500 pages

--- VTiger_KPI_Dashboard/home/views.py
+++ VTiger_KPI_Dashboard/home/views.py
@@ -6,8 +6,8 @@ def home(request):
     '''
     return render(request, "sales/home.html") 
 
-def handler404(request):
-    return render(request, 'sales/404.html', status=404)
+def handler404(request, exception):
+    return render(request, 'sales/404.html')
 
-def handler500(request):
-    return render(request, 'sales/500.html', status=500)
+def handler500(request, exception):
+    return render(request, 'sales/500.html')
