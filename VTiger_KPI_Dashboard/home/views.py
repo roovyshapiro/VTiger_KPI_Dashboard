9ade772c622452f90f0c4cd5c2796d7b0923ab55
Roovy Shapiro <roovyshapiro@gmail.com>
2020-12-24 09:25:10 -0500

    Custom 404,500 revert

--- VTiger_KPI_Dashboard/home/views.py
+++ VTiger_KPI_Dashboard/home/views.py
@@ -5,9 +5,3 @@ def home(request):
     My Home Page.
     '''
     return render(request, "sales/home.html") 
-
-def handler404(request, exception):
-    return render(request, 'sales/404.html')
-
-def handler500(request, exception):
-    return render(request, 'sales/500.html')
--- VTiger_KPI_Dashboard/startapps.sh
+++ VTiger_KPI_Dashboard/startapps.sh
@@ -4,4 +4,4 @@ redis-server &
 celery -A VTiger_KPI_Dashboard worker &
 celery -A VTiger_KPI_Dashboard beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 celery -A VTiger_KPI_Dashboard flower &
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver --insecure 0.0.0.0:8000
