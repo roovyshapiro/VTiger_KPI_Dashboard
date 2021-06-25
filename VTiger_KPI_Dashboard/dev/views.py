abff0dc7d967571cc1f1238dd7cf0e579dda490e
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-25 15:46:07 -0400

    Issues are passed to html & displayed on /dev

--- VTiger_KPI_Dashboard/dev/views.py
+++ VTiger_KPI_Dashboard/dev/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
+from django.db.models import Q
 from .tasks import get_issues
+from .models import Redmine_issues
+import os, json
 
 def main(request):
-    return HttpResponse("Future site of the Dev Dashboard!")
+    all_issues = Redmine_issues.objects.all().order_by('-updated_on')
+    open_issues = all_issues.filter(~Q(status_name="Done"))
+    closed_issues = all_issues.filter(Q(status_name="Done"))
+
+    credentials_file = 'credentials.json'
+    credentials_path = os.path.join(os.path.abspath('.'), credentials_file)
+    with open(credentials_path) as f:
+        data = f.read()
+    credential_dict = json.loads(data)
+    url = credential_dict['redmine_host']
+
+    context ={
+        'all_issues':all_issues,
+        'open_issues':open_issues,
+        'closed_issues':closed_issues,
+        'url': url,
+    }
+    return render(request, "sales/dev.html", context) 
 
 def get_all_issues(request):
     get_issues(recent=False)
