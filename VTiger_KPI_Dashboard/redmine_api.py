30ffe99611da1eeec0eed0293206dadf136fc38d
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-25 12:30:21 -0400

    Get all Redmine issues via API
    This is the 1st commit on the redmine_app branch. The goal is to
    create a new app called "dev" which will track work done by the
    development team on redmine. VTiger is great as a CRM but lacks
    some important features for software development tracking.

---
+++ VTiger_KPI_Dashboard/redmine_api.py
@@ -0,0 +1,52 @@
+#https://www.redmine.org/projects/redmine/wiki/Rest_Issues
+
+import requests, json
+
+class Redmine_API:
+    def __init__(self, username, access_key, host):
+
+        self.username = username
+        self.access_key = access_key
+        self.host = host
+
+    def api_call(self, url):
+        '''
+        Accepts a URL and returns the text
+        '''
+        r = requests.get(url, auth=(self.username, self.access_key))
+
+        r_text = json.loads(r.text)
+        return r_text
+
+    def get_all_data(self):
+        '''
+        Returns data about all modules within VTiger
+        '''
+        data = self.api_call(f"{self.host}/issues.json?status_id=*&limit=100&offset=0")
+        total_count = data['total_count']
+        if total_count < 1000:
+            print('total_count', total_count)
+            return None
+
+        self.all_issues = []
+        for issue in data['issues']:
+            self.all_issues.append(issue)
+
+        while len(self.all_issues) < total_count:
+            data = self.api_call(f"{self.host}/issues.json?status_id=*&limit=100&offset={len(self.all_issues)}")
+            for issue in data['issues']:
+                self.all_issues.append(issue)
+            print("# of issues gathered", len(self.all_issues))
+            print('total_count', total_count)
+        return self.all_issues
+
+
+if __name__ == '__main__':
+    with open('credentials.json') as f:
+        data = f.read()
+    credential_dict = json.loads(data)
+    redmine_api = Redmine_API(credential_dict['redmine_username'], credential_dict['redmine_access_key'], credential_dict['redmine_host'])
+    response = redmine_api.get_all_data()
+    data = json.dumps(response,  indent=4, sort_keys=True)
+    with open('redmine_response.json', 'w') as f:
+        f.write(data)
