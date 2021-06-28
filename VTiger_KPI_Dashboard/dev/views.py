f12eda3f6639c450dd2d2e8b74c267843a810e43
Roovy Shapiro <roovyshapiro@gmail.com>
2021-06-28 11:58:40 -0400

    Redmine Issues by status

--- VTiger_KPI_Dashboard/dev/views.py
+++ VTiger_KPI_Dashboard/dev/views.py
@@ -6,9 +6,23 @@ from .models import Redmine_issues
 import os, json
 
 def main(request):
+    '''
+    Redmine Statuses:
+    Open: New, In Progress, In Test, To Be Determined, Paused
+    Closed: Done, Resolved
+    Rejected: Rejected, Out of Date, Not relevant, Unreproducible, Duplicate
+    '''
+    redmine_issues = {}
     all_issues = Redmine_issues.objects.all().order_by('-updated_on')
-    open_issues = all_issues.filter(~Q(status_name="Done"))
-    closed_issues = all_issues.filter(Q(status_name="Done"))
+    redmine_issues['all_issues'] = all_issues
+    redmine_issues['open_issues'] = all_issues.filter(~Q(status_name="Done") & ~Q(status_name="Resolved") & ~Q(status_name="Rejected") & ~Q(status_name="Out of date") & ~Q(status_name="Not relevant") & ~Q(status_name="Unreproducible") & ~Q(status_name="Duplicate"))
+    redmine_issues['closed_issues'] = all_issues.filter(Q(status_name="Done")  | Q(status_name="Resolved"))
+    redmine_issues['rejected_issues'] = all_issues.filter(Q(status_name="Rejected") | Q(status_name="Out of date") | Q(status_name="Not relevant") | Q(status_name="Unreproducible") | Q(status_name="Duplicate"))
+
+    redmine_issues['all_issues_len'] = len(all_issues)
+    redmine_issues['open_issues_len'] =len(redmine_issues['open_issues'])
+    redmine_issues['closed_issues_len'] = len(redmine_issues['closed_issues'])
+    redmine_issues['rejected_issues_len'] = len(redmine_issues['rejected_issues'])
 
     credentials_file = 'credentials.json'
     credentials_path = os.path.join(os.path.abspath('.'), credentials_file)
@@ -18,9 +32,7 @@ def main(request):
     url = credential_dict['redmine_host']
 
     context ={
-        'all_issues':all_issues,
-        'open_issues':open_issues,
-        'closed_issues':closed_issues,
+        'redmine_issues':redmine_issues,
         'url': url,
     }
     return render(request, "sales/dev.html", context) 
