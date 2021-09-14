9e933a4037f464357ec6be1deec00f99ef1f2bc3
Roovy Shapiro <roovyshapiro@gmail.com>
2021-09-14 12:44:39 -0400

    Created per month line graph change

--- VTiger_KPI_Dashboard/dev/views.py
+++ VTiger_KPI_Dashboard/dev/views.py
@@ -413,7 +413,10 @@ def month_comparison_data(all_issues, issue_status):
 
     #Makes a list of all days in the range of the beginning and end of the available days in db
     for month in comparison_data:
-        date_range = [comparison_data[month]['first_day'] + datetime.timedelta(days=x) for x in range(0, (comparison_data[month]['last_day'] - comparison_data[month]['first_day']).days + 1)]
+        if timezone.now().strftime('%B') == month:
+            date_range = [comparison_data[month]['first_day'] + datetime.timedelta(days=x) for x in range(0, (timezone.now() - comparison_data[month]['first_day']).days + 1)]
+        else:
+            date_range = [comparison_data[month]['first_day'] + datetime.timedelta(days=x) for x in range(0, (comparison_data[month]['last_day'] - comparison_data[month]['first_day']).days + 1)]
         for date in date_range:
             date_count = 0
             if issue_status == "resolved":
@@ -425,6 +428,8 @@ def month_comparison_data(all_issues, issue_status):
                 for issue in all_issues.filter(created_on__gte=comparison_data[month]['first_day'], created_on__lte=comparison_data[month]['last_day']):
                     if issue.created_on.replace(hour=0, minute = 0, second=0,microsecond=0) == date:
                         date_count += 1
+                if len(comparison_data[month]['created']) >= 1:
+                    date_count = date_count + comparison_data[month]['created'][-1]
                 comparison_data[month]['created'].append(date_count)
     return comparison_data
 
